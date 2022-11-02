41b29395d0ac0fffc4cf573595de1999c06b69d0
David Alexander Enamorado Aceituno <daenamorado@bancatlan.hn>
2022-11-01 19:41:45 -0600

    add: mostrar los pokemons con paginacion

--- src/screens/PokemonApp.jsx
+++ src/screens/PokemonApp.jsx
@@ -1,19 +1,37 @@
 import { useEffect } from "react"
-import { useDispatch } from "react-redux"
+import { useDispatch, useSelector } from "react-redux"
 import { getPokemons } from "../store/slices/pokemon/thunks";
 
 export const PokemonApp = () => {
 
+  const { isLoading, pokemons, page } = useSelector(state => state.pokemons);
   const dispatch = useDispatch();
 
 
   useEffect(() => {
-    dispatch(getPokemons());
+    dispatch(getPokemons(page));
   }, [])
 
 
   return (
     <>
+      <h1>Pokemon App</h1>
+      <hr />
+      <span>Loagind: {isLoading ? 'true' : 'false'}</span>
+
+      <ul>
+        {
+          pokemons.map(pokemon => (
+            <li key={pokemon.name}>{pokemon.name}</li>
+          ))
+        }
+      </ul>
+      <button
+        disabled={isLoading}
+        onClick={() => dispatch(getPokemons(page))}
+      >
+        Next
+      </button>
 
     </>
   )
